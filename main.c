e0caa76eddf1e73ac10786202cdb41c7f4cfede2
Ershova Maria <113013796+0maffina0@users.noreply.github.com>
2022-11-17 11:57:31 +0300

    First, Second and Third PR

--- main.c
+++ main.c
@@ -2,30 +2,19 @@
 
 int main()
 {
-    const int amax = 1000;
-    int n[amax], key1, key2, j, i;
+    int n[36];
+    int i, j, key;
     scanf("%d", &j);
 
-    for( i = 0; i < j; i++)
+    for(i = 0; i < j; i++)
         scanf("%d", &n[i]);
-    int k = 0;
-    key1 = n[0];
-    key2 = 2000000000;
-    for(int i = 0; i < j; i++) {
-        if (n[i] < key1) {
-            key1 = n[i];
-            k = i;
-        }
-    }
-    for(int i = 0; i < j; i++){
-        if (n[i] < key2 && n[i] >= key1 && k != i){
-            key2 = n[i];
-        }
-    }
-        if (n[i] < key2 & n[i] > key1)
-            key2 = n[i];
 
-    printf("%d ", key1);
-    printf("%d ", key2);
+    key = n[j - 1];
+    for (int i = j; i > 0; i--)
+        n[i] = n[i - 1];
+    n[0] = key;
+
+    for(i = 0; i < j; i++)
+        printf("%d ", n[i]);
     return 0;
 }
